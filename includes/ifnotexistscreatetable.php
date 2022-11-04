172a2938b6f45da26d66f8e1cf4a49adf104d117
Woga65 <woga@mytsa.de>
2022-11-05 00:52:42 +0100

    retrieve user's preferred language(s) from server

--- includes/ifnotexistscreatetable.php
+++ includes/ifnotexistscreatetable.php
@@ -2,7 +2,20 @@
 
 require 'dbh.inc.php';
 
-$state = '{ "err": "", "ok": true, "msg": "table exists", "data":{ "loggedIn":false } }';
+$langs = array();
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $lang_parse);
+    if (count($lang_parse[1])) {
+        $langs = array_combine($lang_parse[1], $lang_parse[4]);
+        foreach ($langs as $lang => $val) {
+            $langs[$lang] = ($val === '') ? 1 : $val;
+        }
+        arsort($langs, SORT_NUMERIC);
+    }
+}
+
+$langs = '"langs": ' . json_encode(array_keys($langs));
+$state = '{ "err": "", "ok": true, "msg": "table exists", ' . $langs . ', "data": { "loggedIn": false } }';
 
 $sql = "DESCRIBE `kb_users`;";
 
@@ -19,9 +32,9 @@ if (!mysqli_query($conn, $sql)) {
         PRIMARY KEY (`user_id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
     if (!mysqli_query($conn, $sql)) {
-        $state ='{ "err": "sql error: ' . mysqli_error($conn) . '", "ok": false, "data": { "loggedIn": false } }';
+        $state ='{ "err": "sql error: ' . mysqli_error($conn) . '", "ok": false, ' . $langs . ', "data": { "loggedIn": false } }';
     } else {
-        $state = '{ "err": "", "ok": true, "msg": "table created", "data":{ "loggedIn":false } }';
+        $state = '{ "err": "", "ok": true, "msg": "table created", ' . $langs . ', "data": { "loggedIn": false } }';
     }
 }
 
